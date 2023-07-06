12fd9c84bd8500c4890474f9d32fca9700c39d59
Youenn Gasc <59448502+Treldyr@users.noreply.github.com>
2023-07-06 16:30:03 +0200

    Update countWordsCaller.groovy
    added parameters

--- .github/workflows/countWordsCaller.groovy
+++ .github/workflows/countWordsCaller.groovy
@@ -1,18 +1,18 @@
 pipeline {
     agent any
 
+    parameters {
+        string(name: 'MOT', description: 'Le mot qui sera affiché')
+        string(name: 'NOMBRE', description: 'Le nombre de fois que ce mot sera affiché')
+    }
+
     stages {
-
         stage('Clonage git') {
-            steps {
-                
+            steps {   
                 checkout([$class: 'GitSCM', branches: [[name: '*/master']], userRemoteConfigs: [[url: 'https://github.com/Treldyr/jenkins_files.git']]])
-
-                sh '''
-                    /bin/python3 .github/workflows/countWordsCaller.py 3--bamboo
-                '''
+                sh "/bin/python3 .github/workflows/countWordsCaller.py ${params.NOMBRE}--${params.MOT}"
+                
             }
         }
-
     }
 }
