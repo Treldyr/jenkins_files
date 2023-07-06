daf2360e3d753d4675eb4cc4974aa553b0e92ada
Youenn Gasc <59448502+Treldyr@users.noreply.github.com>
2023-07-06 16:35:44 +0200

    Update countWordsCaller.groovy
    rectification naming file+accent

--- .github/workflows/countWordsCaller.groovy
+++ .github/workflows/countWordsCaller.groovy
@@ -2,15 +2,15 @@ pipeline {
     agent any
 
     parameters {
-        string(name: 'MOT', description: 'Le mot qui sera affiché')
-        string(name: 'NOMBRE', description: 'Le nombre de fois que ce mot sera affiché')
+        string(name: 'MOT', description: 'Le mot qui sera affiche')
+        string(name: 'NOMBRE', description: 'Le nombre de fois que ce mot sera affiche')
     }
 
     stages {
         stage('Clonage git') {
             steps {   
                 checkout([$class: 'GitSCM', branches: [[name: '*/master']], userRemoteConfigs: [[url: 'https://github.com/Treldyr/jenkins_files.git']]])
-                sh "/bin/python3 .github/workflows/countWordsCaller.py ${params.NOMBRE}--${params.MOT}"
+                sh "/bin/python3 .github/workflows/countWordsProgram.py ${params.NOMBRE}--${params.MOT}"
                 
             }
         }
