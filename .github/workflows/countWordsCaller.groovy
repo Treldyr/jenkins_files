d95ecf7e222a64e146bbc80b6881ae3afc9bcb3b
Youenn Gasc <59448502+Treldyr@users.noreply.github.com>
2023-07-06 16:14:12 +0200

    Update countWordsCaller.groovy
    added working caller in groovy

--- .github/workflows/countWordsCaller.groovy
+++ .github/workflows/countWordsCaller.groovy
@@ -1,1 +1,18 @@
-// this will be edited
+pipeline {
+    agent any
+
+    stages {
+
+        stage('Clonage git') {
+            steps {
+                
+                checkout([$class: 'GitSCM', branches: [[name: '*/master']], userRemoteConfigs: [[url: 'https://github.com/Treldyr/jenkins_files.git']]])
+
+                sh '''
+                    /bin/python3 .github/workflows/countWordsCaller.py 3--bamboo
+                '''
+            }
+        }
+
+    }
+}
